f1d201549e8d95a4603a729ebefeb25ab1de37b5
Carolina <79524656+carochacs@users.noreply.github.com>
2023-10-21 10:22:16 -0600

    Name

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
 
 export const BRAND = "Tresce Design Collective";
 export const SITE_TITLE = "Tresce DC";
-export const SITE_URL = "tresce.co";
-export const SITE_DESCRIPTION = "Welcome to our Design & Engineering blog!";
+export const SITE_URL = "https://tresce.co";
+export const SITE_DESCRIPTION = "Welcome to our Engineering blog!";
 export const PAGINATION_POSTS_PER_PAGE = 5;
